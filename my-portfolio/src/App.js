c3e7481c27960b062a9c55245dafa55eb1318e45
Meelyn Pandit <meelyn.pandit@gmail.com>
2025-10-13 10:14:45 -0400

    branch init, attempt at integrating vite, chakra-ui, and react-router

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -6,6 +6,7 @@ import Projects from "./components/projects";
 import Skills from "./components/skills";
 import ResearchInterests from "./components/research_interests";
 
+
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
